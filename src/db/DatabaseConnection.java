1293cf98ae8f9428f2cd42fb4ff13ae872400180
bandola78 <165742856+bandola78@users.noreply.github.com>
2024-12-21 04:58:37 +0800

    Update DatabaseConnection.java

--- src/db/DatabaseConnection.java
+++ src/db/DatabaseConnection.java
@@ -6,16 +6,11 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/Stray_Haven";
-    private static final String USER = "root";  
-    private static final String PASSWORD = "yourpassword";  
-
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new SQLException("Database connection failed: " + e.getMessage());
-        }
+        String url = "jdbc:mysql://localhost/strayhaven";
+        String user = "root";
+        String password = "kAKrR_9n"; 
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
